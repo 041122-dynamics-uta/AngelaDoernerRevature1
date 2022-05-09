28aa01e03a14cb3aedaa48e8054827e821a7eda7
Angela-Thiessen <Angela-Thiessen@users.noreply.github.com>
2022-05-09 08:51:48 -0400

    HackerRank Weather Observation 10

---
+++ w4/w4/weatherObservation10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weatherObservation10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
